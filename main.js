bb4aeedcb064f5fa306f43f84e9a9b31af31ab66
katrinasuherman <ksuherman@ucsd.edu>
2025-05-05 12:26:05 -0700

    fixposition

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 const margin = { top: 50, right: 100, bottom: 50, left: 100 };
-const width = 2000 - margin.left - margin.right;
+const width = 1200 - margin.left - margin.right;
 const height = 600 - margin.top - margin.bottom;
 
 const svg = d3
   .select("#parallelPlot")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
+  .style("display", "block")
+  .style("margin-left", "0")
   .append("g")
   .attr("transform", `translate(${margin.left},${margin.top})`);
 
-const dimensions = ["age",  "weight", "op_duration_hr"];
+const dimensions = ["age", "death_inhosp", "weight", "op_duration_hr"];
 const colorMap = {
-  Open: "#1f77b4",
-  Robotic: "#9467bd",
-  Videoscopic: "#2ca02c"
+  Open: "#e41a1c",        // red
+  Robotic: "#377eb8",     // blue
+  Videoscopic: "#4daf4a"   // green
 };
 
 // Load your local CSV file
 d3.csv("with_opdur.csv").then((data) => {
   data.forEach((d) => {
     d.age = +d.age;
-    //d.death_inhosp = +d.death_inhosp;
+    d.death_inhosp = +d.death_inhosp;
     d.weight = +d.weight;
     d.op_duration_hr = +d.op_duration_hr;
   });
@@ -68,24 +70,25 @@ d3.csv("with_opdur.csv").then((data) => {
       .text(dim);
   });
 
+  // Add legend
   const legend = svg.append("g")
-  .attr("class", "legend")
-  .attr("transform", `translate(${width - 150}, 0)`);
+    .attr("class", "legend")
+    .attr("transform", `translate(${width - 150}, 0)`);
 
-Object.entries(colorMap).forEach(([label, color], i) => {
-  const legendRow = legend.append("g")
-    .attr("transform", `translate(0, ${i * 20})`);
+  Object.entries(colorMap).forEach(([label, color], i) => {
+    const legendRow = legend.append("g")
+      .attr("transform", `translate(0, ${i * 20})`);
 
-  legendRow.append("rect")
-    .attr("width", 12)
-    .attr("height", 12)
-    .attr("fill", color);
+    legendRow.append("rect")
+      .attr("width", 12)
+      .attr("height", 12)
+      .attr("fill", color);
 
-  legendRow.append("text")
-    .attr("x", 18)
-    .attr("y", 10)
-    .attr("font-size", "12px")
-    .attr("fill", "black")
-    .text(label);
-});
+    legendRow.append("text")
+      .attr("x", 18)
+      .attr("y", 10)
+      .attr("font-size", "12px")
+      .attr("fill", "black")
+      .text(label);
+  });
 });
